2dae70b2a96b9f1b0b4c82307fe2ff38965866e5
nsh29 <nurseto@students.amikom.ac.id>
2025-03-11 14:05:47 +0700

    melengkapi fungsi main

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,11 @@ class Calulator
 {
 	static void Main(string[] args)
 	{
+		int a=10;
+		int b=6;
 		
+		Console.Writeline("Hasil Penambahan: {0}+{1}={2}",a,b,Penambahan(a,b));
+		Console.Writeline("Hasil Pengurangan: {0}-{1}={2}",a,b,Pengurangan(a,b));
 	}
 	static int Penambahan(int a, int b)
 	{
